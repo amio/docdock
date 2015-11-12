2deffbb86b2daf88a8c8b51d280b095b4b48fd90
Amio <amio.cn@gmail.com>
2015-11-12 15:44:53 +0800

    Remember window position

--- app/background.js
+++ app/background.js
@@ -1,5 +1,6 @@
 chrome.app.runtime.onLaunched.addListener(function () {
   chrome.app.window.create('dock.html', {
+    'id': 'dock',
     'outerBounds': {
       'width': 1024,
       'height': 640
